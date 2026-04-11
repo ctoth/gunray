7a78b2ce306d5530e7bcdeeb8f54b611dcbca2c8
Christopher Toth <q.alpha@gmail.com>
2026-04-11 12:44:00 -0600

    Export trace types from package surface

--- src/gunray/__init__.py
+++ src/gunray/__init__.py
@@ -3,5 +3,14 @@
 from .adapter import GunrayEvaluator
 from .defeasible import DefeasibleEvaluator
 from .evaluator import SemiNaiveEvaluator
+from .trace import ClassificationTrace, DatalogTrace, DefeasibleTrace, ProofAttemptTrace
 
-__all__ = ["DefeasibleEvaluator", "GunrayEvaluator", "SemiNaiveEvaluator"]
+__all__ = [
+    "ClassificationTrace",
+    "DatalogTrace",
+    "DefeasibleEvaluator",
+    "DefeasibleTrace",
+    "GunrayEvaluator",
+    "ProofAttemptTrace",
+    "SemiNaiveEvaluator",
+]
