75d504eb0044f1ed11b00990b6004f3be2177648
Christopher Toth <q.alpha@gmail.com>
2026-04-13 18:03:57 -0600

    test(disagreement): unrelated literals do not disagree

--- tests/test_disagreement.py
+++ tests/test_disagreement.py
@@ -10,3 +10,9 @@ def test_disagrees_on_complementary_literals() -> None:
     flies_tweety = GroundAtom(predicate="flies", arguments=("tweety",))
     not_flies_tweety = GroundAtom(predicate="~flies", arguments=("tweety",))
     assert disagrees(flies_tweety, not_flies_tweety, ()) is True
+
+
+def test_does_not_disagree_on_unrelated_literals() -> None:
+    flies_tweety = GroundAtom(predicate="flies", arguments=("tweety",))
+    bird_tweety = GroundAtom(predicate="bird", arguments=("tweety",))
+    assert disagrees(flies_tweety, bird_tweety, ()) is False
